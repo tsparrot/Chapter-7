823c705f6b08f122bc07f3b41156f9349065d7a6
tsparrot <1134879109@qq.com>
2020-06-11 19:01:00 +0800

    安卓作业七

--- assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
+++ assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -1,19 +1,73 @@
 package com.bytedance.videoplayer;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
+    private VideoView mvideoView;
+    private int videoPosition = 0;
+    private boolean isPlaying = false;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imageView);
-        String url = "https://s3.pstatp.com/toutiao/static/img/logo.271e845.png";
-        Glide.with(this).load(url).into(imageView);
+        mvideoView = findViewById(R.id.mvideoView);
+        mvideoView.setVideoPath(getVideoPath(R.raw.mytest));
+        MediaController videoController = new MediaController(this);
+        mvideoView.setMediaController(videoController);
+        videoController.setMediaPlayer(mvideoView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        videoPosition = mvideoView.getCurrentPosition();
+        isPlaying = mvideoView.isPlaying();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mvideoView.seekTo(videoPosition);
+        if (isPlaying)
+            mvideoView.start();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@Nullable Bundle state) {
+
+        if (state != null) {
+            int position = videoPosition;
+            state.putInt("position", position);
+            state.putBoolean("isPlaying", isPlaying);
+        }
+        super.onSaveInstanceState(state);
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(@Nullable Bundle state) {
+        super.onRestoreInstanceState(state);
+        if (state != null) {
+            int position = state.getInt("position");
+            boolean isplaying = state.getBoolean("isPlaying");
+            videoPosition = position;
+            mvideoView.seekTo(position);
+            isPlaying = isplaying;
+            if (isPlaying)
+                mvideoView.start();
+        }
+
+    }
+
+    private String getVideoPath(int resId) {
+        return "android.resource://" + this.getPackageName() + "/" + resId;
     }
 }
